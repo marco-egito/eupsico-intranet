1f1e64a99119e233bfd9750eec7e9a48e773d190
marco-egito <aureliog.t.i@gmail.com>
2025-08-29 18:31:16 -0300

    commit

---
+++ js/firebase-config.js
@@ -0,0 +1,23 @@
+// js/firebase-config.js
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+
+// Objeto de configuração do seu projeto Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyCLeWW39nqxsdv1YD-CNa9RSTv05lGHJxM",
+    authDomain: "eupsico-agendamentos-d2048.firebaseapp.com",
+    databaseURL: "https://eupsico-agendamentos-d2048-default-rtdb.firebaseio.com",
+    projectId: "eupsico-agendamentos-d2048",
+    storageBucket: "eupsico-agendamentos-d2048.appspot.com",
+    messagingSenderId: "1041518416343",
+    appId: "1:1041518416343:web:3a0abc73404ccd51d7bb92"
+};
+
+// Inicializa o app
+const app = initializeApp(firebaseConfig);
+
+// Exporta os serviços que serão usados em outras partes do código
+export const auth = getAuth(app);
+export const db = getFirestore(app);
