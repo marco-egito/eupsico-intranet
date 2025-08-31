964731eea5f7cb02db91a44f5ec49cb9b8bb6f5d
marco-egito <128816469+marco-egito@users.noreply.github.com>
2025-08-31 19:09:22 -0300

    Create firebase-keys.js

---
+++ js/firebase-keys.js
@@ -0,0 +1,12 @@
+// ATENÇÃO: Este arquivo contém informações sensíveis.
+// Ele foi adicionado ao .gitignore para não ser enviado ao repositório.
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCLeWW39nqxsdv1YD-CNa9RSTv05lGHJxM",
+  authDomain: "eupsico-agendamentos-d2048.firebaseapp.com",
+  databaseURL: "https://eupsico-agendamentos-d2048-default-rtdb.firebaseio.com",
+  projectId: "eupsico-agendamentos-d2048",
+  storageBucket: "eupsico-agendamentos-d2048.firebasestorage.app",
+  messagingSenderId: "1041518416343",
+  appId: "1:1041518416343:web:3a0abc73404ccd51d7bb92"
+};
