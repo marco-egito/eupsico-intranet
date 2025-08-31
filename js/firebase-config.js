3e7682de34d99d6a5ee8061d072be5e9dfb612cc
marco-egito <128816469+marco-egito@users.noreply.github.com>
2025-08-31 17:51:26 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -6,13 +6,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase
 
 // Objeto de configuração do seu projeto Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyCLeWW39nqxsdv1YD-CNa9RSTv05lGHJxM",
-    authDomain: "eupsico-agendamentos-d2048.firebaseapp.com",
-    databaseURL: "https://eupsico-agendamentos-d2048-default-rtdb.firebaseio.com",
-    projectId: "eupsico-agendamentos-d2048",
-    storageBucket: "eupsico-agendamentos-d2048.appspot.com",
-    messagingSenderId: "1041518416343",
-    appId: "1:1041518416343:web:3a0abc73404ccd51d7bb92"
+  apiKey: "AIzaSyCLeWW39nqxsdv1YD-CNa9RSTv05lGHJxM",
+  authDomain: "eupsico-agendamentos-d2048.firebaseapp.com",
+  databaseURL: "https://eupsico-agendamentos-d2048-default-rtdb.firebaseio.com",
+  projectId: "eupsico-agendamentos-d2048",
+  storageBucket: "eupsico-agendamentos-d2048.firebasestorage.app",
+  messagingSenderId: "1041518416343",
+  appId: "1:1041518416343:web:3a0abc73404ccd51d7bb92"
 };
 
 // Inicializa o app
